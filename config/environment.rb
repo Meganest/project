aa7c7548dc393093f8becfae502a884652da4d89
xmoss <my.xmoss@gmail.com>
2015-07-22 10:32:05 +0000

    fixed

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,8 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings ={
+config.action_mailer.default_options = {from: "my.xmoss@gmail.com" }
+ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port =>'587',
     :authentication => :plain,
